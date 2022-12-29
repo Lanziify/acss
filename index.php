8af6c8a864b3fa1f7a45b1635e8fc566757cb397
Pyuuu <papasin.lancel@gmail.com>
2022-12-29 16:20:34 +0800

    commit

--- index.php
+++ index.php
@@ -13,20 +13,37 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        body {
-            font: 14px sans-serif;
-            text-align: center;
-        }
     </style>
 </head>
 
 <body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["Firstname"]); ?></h1>
-    <p>
-        <a href="logout.php" class="btn btn-danger ml-3">Logout</a>
-    </p>
+    <div class="index-main-wrapper">
+
+        <div class="header">
+
+        </div>
+        <div class="wrap-main-page">
+            <div class="side-menu">
+                <div class="items-top">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul>
+                </div>
+                <div class="items-bottom">
+                    <a href="#">
+                        <?php echo htmlspecialchars($_SESSION["Firstname"]); ?>
+                    </a>
+                    <hr>
+                    <a href="logout.php" class="logout-button">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <script src="script.js"></script>
 </body>
 
 </html>
